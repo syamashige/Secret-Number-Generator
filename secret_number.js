e4c9967ca3cd5309ccc91cf09d35b56258106225
Sarah <yamashige.sarah@gmail.com>
2018-07-07 15:18:51 -1000

    finished

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,27 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function () {
+  const secretNumber = Math.floor(Math.random() * Math.floor(1000001));
+  function secret(){
+      console.log(secretNumber);
+      return secretNumber;
+  }
+  return secret;
+    // function secretNumber () {
+    //     const randomNum = Math.floor(Math.random() * Math.floor(1000000));
+    //     return function () {
+    //         return randomNum;
+    //     }
+    // }
+    // const getSecretNum = secretNumber();
+
+    // console.log(getSecretNum());
 };
+
+
+
+
+
+
+// const getSecretNum = makeSecretNum();
+
+// console.log(getSecretNum());
